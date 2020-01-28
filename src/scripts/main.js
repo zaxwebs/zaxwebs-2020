4fa94428385b467f641ac31ffb8df58c35c1d340
zaxwebs <zifidigital@gmail.com>
2020-01-28 21:59:13 +0530

    Added Parallax to home/cover elements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 import $ from 'jquery'
 import 'bootstrap'
+import Rellaxx from './rellaxx'
 import Rellax from 'rellax'
+
+const zack = new Rellaxx('#zack', {
+	speed: 4
+})
+
+const webster = new Rellaxx('#webster', {
+	speed: -3.5
+})
+
+const intro = new Rellaxx('#home-intro', {
+	speed: -4
+})
