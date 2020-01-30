521821885b5d14e8ca95b72c966f13f32b628dbd
zaxwebs <zifidigital@gmail.com>
2020-01-30 22:54:51 +0530

    Removed rellax.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,2 @@
 import $ from 'jquery'
 import 'bootstrap'
-import Rellaxx from './rellaxx'
-import Rellax from 'rellax'
-
-const zack = new Rellaxx('#zack', {
-	speed: 4
-})
-
-const webster = new Rellaxx('#webster', {
-	speed: -3.5
-})
-
-const intro = new Rellaxx('#home-intro', {
-	speed: 0
-})
-
-const square = new Rellaxx('#square', {
-	speed: 3
-})
