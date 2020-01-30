394799c21e1dd1b7ac306c3f47668abe53b64758
zaxwebs <zifidigital@gmail.com>
2020-01-30 23:22:46 +0530

    Completed Lax v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@ import 'bootstrap'
 import lax from 'lax.js'
 
 window.onload = function() {
-	lax.setup() // init
+	lax.setup({ breakpoints: { small: 0, large: 992 } }) // init
 
 	const updateLax = () => {
 		lax.update(window.scrollY)
