e443806eacf105d1c221b2fff60e6440d33102ec
zaxwebs <zifidigital@gmail.com>
2020-01-30 23:07:23 +0530

    Added lax setup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,14 @@
 import $ from 'jquery'
 import 'bootstrap'
+import lax from 'lax.js'
+
+window.onload = function() {
+	lax.setup() // init
+
+	const updateLax = () => {
+		lax.update(window.scrollY)
+		window.requestAnimationFrame(updateLax)
+	}
+
+	window.requestAnimationFrame(updateLax)
+}
